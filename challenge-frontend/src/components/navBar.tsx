5c251eaff1e7076d514d30b2be6374797666bed6
agusala <agustinsala0@gmail.com>
2025-02-20 12:39:30 -0300

    initial app

---
+++ challenge-frontend/src/components/navBar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Nav Bar</h1>
+    </div>
+  );
+};
+export default Navbar;
---
+++ challenge-frontend/src/pages/home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Esta é a página inicial</p>
+    </div>
+  );
+};
+export default Home;
